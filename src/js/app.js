63ce1f11a674e0e71c7808ad5e62946aefc28fc4
sartios <sartios@hotmail.com>
2016-09-09 18:24:21 +0300

    fix angular injection for uglify

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@
   'use strict';
 
   angular.module('myApp', ['ngRoute'])
-    .config(function($routeProvider){
-      $routeProvider.when('/home', {
-        templateUrl: 'partials/home.html'
-      }).when('/user', {
-        templateUrl: 'partials/user.html'
-      }).otherwise('/');
-    });
+    .config(['$routeProvider',
+      function($routeProvider){
+        $routeProvider.when('/home', {
+          templateUrl: 'partials/home.html'
+        }).when('/user', {
+          templateUrl: 'partials/user.html'
+        }).otherwise('/');
+      }
+    ]);
 })();
